9f22e96439b5870603df5a0428d067e88b796374
Nytheneals <nythes@gmail.com>
2018-02-28 08:19:59 -0500

    Map reduce and Filter Practice

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: ['M', 'E', 'R', 'N'],
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {this.state.name.map((n, i) => <h2 key={i}>{n}</h2>)}
+        {this.state.name.reduce((prev, next) => prev + next, [])}
+        <h1>Welcome to React</h1>
       </div>
     );
   }
